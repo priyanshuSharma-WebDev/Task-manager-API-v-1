7787c0f353b7a67e47ec3f0efb6300b0593cd633
Priyanshu sharma <priyanshushrama709@gmail.com>
2020-10-08 21:54:09 +0530

    Update sendingEmail.js

--- src/routes/sendingEmail.js
+++ src/routes/sendingEmail.js
@@ -1,4 +1,7 @@
-const express = require("express");
-const { UserModel, TaskModel } = require("../DB/models");
-const router = new express.Router();
-const Authentication = require("../auth/auth");
+// const express = require("express");
+// const { UserModel, TaskModel } = require("../DB/models");
+// const router = new express.Router();
+// const Authentication = require("../auth/auth");
+
+
+// this file is not ready yet I will work on it very soon:D
